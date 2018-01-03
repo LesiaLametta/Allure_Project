62adf5ec88a9a513f7d149124b486d6b007465f6
Lesia Lametta <lesialametta@gmail.com>
2018-01-03 09:55:31 +0300

    Project Init

---
+++ js/init.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    items: 1,
+    nav: true,
+    navText: ['<i class="material-icons">keyboard_arrow_left</i>','<i class="material-icons">keyboard_arrow_right</i>']
+  });
+});
