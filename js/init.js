f7d83f7b84290370254f4891e05f6531c2d56a2b
Lesia Lametta <lesialametta@gmail.com>
2018-01-05 11:21:16 +0300

    Slider Implementation (Rought Copy)

--- js/init.js
+++ js/init.js
@@ -1,7 +1,14 @@
-$(document).ready(function(){
-  $(".owl-carousel").owlCarousel({
-    items: 1,
-    nav: true,
-    navText: ['<i class="material-icons">keyboard_arrow_left</i>','<i class="material-icons">keyboard_arrow_right</i>']
-  });
+$(document).ready(function() {
+    $(".slider").owlCarousel({
+        items: 1,
+        nav: true,
+        navText: ['<i class="material-icons">keyboard_arrow_left</i>', '<i class="material-icons">keyboard_arrow_right</i>']
+    });
+    
+    $(".sliderr").owlCarousel({
+        items: 5,
+        nav: true,
+        dots: true,
+        navText: ['<i class="material-icons">keyboard_arrow_left</i>', '<i class="material-icons">keyboard_arrow_right</i>'],      
+    });
 });
