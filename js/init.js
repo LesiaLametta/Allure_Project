051e875094acd2bfe67a0ce07f7455f9900ef6ba
Lesia Lametta <lesialametta@gmail.com>
2018-01-11 10:08:25 +0300

    Code Clean Up

--- js/init.js
+++ js/init.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     $(".slider").owlCarousel({
         items: 1,
+        loop: true,
         nav: true,
         navText: ['<i class="material-icons">keyboard_arrow_left</i>', '<i class="material-icons">keyboard_arrow_right</i>']
     });
